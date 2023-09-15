bfbe865c40dc2914faa2ae27e4b96b14ac69d47f
SeanMichaelRyan <seanmichaelryan@github.com>
2023-09-15 07:44:59 +0000

    Configured ports for index.js and moved in dir

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files from a directory (e.g., CSS, images, etc.)
+app.use(express.static('csc648-03-fa23-team06')); // Create a 'public' directory for your static files
+
+// Define a route to serve your main HTML page
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/application/about-landing/aboutMe-landing.html');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+//app.get('/', (req, res) => {res.send('HEY!')})
+
