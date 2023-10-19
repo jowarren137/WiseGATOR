84df301f6402fdf34bda6eec461bcec9180b9b83
SeanMichaelRyan <seanmichaelryan@github.com>
2023-10-19 03:52:07 +0000

    Server Connects

--- application/conf/database.js
+++ application/conf/database.js
@@ -1,7 +1,7 @@
 //Database Connection
 const mysql = require('mysql');
 
-const pool  = mysql.createPool({
+const db  = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'GatorsTeam6!',
@@ -9,13 +9,13 @@ const pool  = mysql.createPool({
   queueLimit: 0,
   waitForConnections: true
 });
-//pool.connect((err) => {
-//  if (err)
- //       {
-  //      console.log('Error connecting to database!');
-      //  throw err;
-    //    }
-        // console.log('Connected to database!');
-//});
+db.connect((err) => {
+  if (err)
+      {
+        console.log('Error connecting to database!');
+        throw err;
+        }
+         console.log('Connected to database!');
+});
 
-module.exports = pool;
+module.exports = db;
