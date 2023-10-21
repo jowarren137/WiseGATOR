7147b61ae07733e4e6b9799422550158df7b3b49
karlMoreno <karlcmoreno0811@gmail.com>
2023-10-21 01:01:59 -0700

    re run of database

--- application/bin/dbloader.sql
+++ application/bin/dbloader.sql
@@ -3,24 +3,45 @@
 CREATE DATABASE gatorDB;
 
 USE gatorDB;
-    
-    CREATE TABLE tutors (
-      id INT UNSIGNED NOT NULL,
-      name VARCHAR(100) NOT NULL,
-      topic VARCHAR(4) NOT NULL,
-      description TEXT,
-      flyer VARCHAR(50),
-      picture VARCHAR(50),
-      video VARCHAR(50)
-  );
 
-    INSERT INTO tutors (id, name, topic)
+CREATE TABLE `topics` (
+  `subject` VARCHAR(4) NOT NULL,
+  PRIMARY KEY (`subject`),
+  UNIQUE INDEX `subject_UNIQUE` (`subject` ASC) VISIBLE);
+
+CREATE TABLE `tutors` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `subject_id` VARCHAR(4) NOT NULL,
+  `description` TEXT NULL,
+  `flyer` VARCHAR(50) NULL,
+  `picture` VARCHAR(50) NULL,
+  `video` VARCHAR(50) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  INDEX `fk_topic_idx` (`subject_id` ASC) VISIBLE,
+  CONSTRAINT `fk_topic`
+    FOREIGN KEY (`subject_id`)
+    REFERENCES `topics` (`Subject`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+ INSERT INTO topics (subject)
     VALUES
-	(112345678, 'Philip Karnatsevich', 'CSC'),
-	(123456789, 'Joaquin Warren', 'BUS'),
-	(234567891, 'Ronnie Huang', 'ASTR'),
-	(345678912, 'Karl Moreno', 'DM'),
-	(456789123, 'Darien Banuelos', 'ARTH'),
-	(567891234), 'Sean Ryan', 'CSC'),
-	(678912345), 'Joe Schmuck', 'MATH'),
-	(789123456), 'John Doe', 'CSC'); 
+	 ('ARTH'),
+	('ASTR'),
+	('BUS'),
+	 ('CSC'),
+	('DM'),
+	('MATH');
+
+    INSERT INTO tutors (id, name, subject_id)
+    VALUES
+	(1, 'Philip Karnatsevich', 'CSC'),
+	(2, 'Joaquin Warren', 'BUS'),
+	(3, 'Ronnie Huang', 'ASTR'),
+	(4, 'Karl Moreno', 'DM'),
+	(5, 'Darien Banuelos', 'ARTH'),
+	(6, 'Sean Ryan', 'CSC'),
+	(7, 'Joe Schmuck', 'MATH'),
+	(8, 'John Doe', 'CSC'); 
