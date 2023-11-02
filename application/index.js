dac98d783b9dffdfa00976c739074860399d84e7
SeanMichaelRyan <47589320+SeanMichaelRyan@users.noreply.github.com>
2023-11-01 22:43:58 -0700

    Moved database call and created route for users.js
    organizing structure

--- application/index.js
+++ application/index.js
@@ -4,22 +4,7 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-
-const db  = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  port: '3306',
-  password: '',
-  database: 'gatorDB'
-});
-db.connect((err) => {
-  if (err)
-      {
-        console.log('Error connecting to database!');
-        throw err;
-        }
-         console.log('Connected to database!');
-});
+const db = require('./conf/database.js');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
