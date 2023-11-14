5903f168f662a5da85d1e0e6d9aec9f06d592917
SeanMichaelRyan <47589320+SeanMichaelRyan@users.noreply.github.com>
2023-11-13 23:59:53 -0800

    Restructured, fixed static directory, added search to home.
    -Renamed index to app.js and moved view pages to be routed in routes/index
    -Webpage static directory should now work as intended
    -Search function is on homepage but needs alignment
    -converted html to ejs

---
+++ application/app.js
@@ -0,0 +1,95 @@
+const { Console } = require('console');
+const express = require('express');
+const app = express();
+const port = 3000;
+const usersRouter = require("./routes/users.js");
+const indexRouter = require("./routes/index.js");
+const path = require("path");
+//Body parser middleware
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+// set the view engine to ejs
+app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+
+
+
+// Serve static files from a directory (e.g., CSS, images, etc.)
+// Create a 'public' directory for your static files
+app.use("/webpage", express.static(path.join(__dirname, "webpage")));
+
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+//Routes
+
+app.get('/ronnie-page', (req,res) => {
+    res.sendFile(__dirname + '/webpage/about-pages/ronnie.html');
+});
+
+app.get('/joaquin-page', (req,res) => {
+    res.sendFile(__dirname + '/webpage/about-pages/joaquin.html');
+});
+
+app.get('/karl-page', (req,res) => {
+    res.sendFile(__dirname + '/webpage/about-pages/karl.html');
+});
+
+app.get('/sean-page', (req,res) => {
+    res.sendFile(__dirname + '/webpage/about-pages/sean.html');
+});
+
+app.get('/philip-page', (req,res) => {
+    res.sendFile(__dirname + '/webpage/about-pages/philip.html');
+});
+
+app.get('/darien-page', (req,res) => {
+    res.sendFile(__dirname + '/webpage/about-pages/darien.html');
+});
+
+//IMG routes
+
+app.get('/landing-group-img/', (req,res) => {
+    res.sendFile(__dirname + '/webpage/about-pages/abt-imgs/group.png');
+});
+
+app.get('/ronnie-about-img/', (req,res) => {
+    res.sendFile(__dirname + '/webpage/about-pages/abt-imgs/ronnieAbt.jpg');
+});
+app.get('/joaquin-about-img/', (req,res) => {
+    res.sendFile(__dirname + '/webpage/about-pages/abt-imgs/joaquinAbt.jpg');
+});
+
+app.get('/philip-about-img/', (req,res) => {
+    res.sendFile(__dirname + '/webpage/about-pages/abt-imgs/philipAbt.jpg');
+});
+app.get('/darien-about-img/', (req,res) => {
+    res.sendFile(__dirname + '/webpage/about-pages/abt-imgs/darienAbt.jpg');
+});
+app.get('/default-about-img/', (req,res) => {
+    res.sendFile(__dirname + '/webpage/about-pages/abt-imgs/defaultAbt.jpg');
+});
+
+app.get('/about-us/', function (req, res) {
+    res.sendFile(__dirname + '/webpage/about-pages/aboutMe-landing.html');
+});
+// Page routes
+
+// app.get('/home-page/', function (req, res) {
+//     res.sendFile(__dirname + '/webpage/home/home.html');
+// });
+
+app.use("/users", usersRouter);
+app.use("/", indexRouter);
+
+
+
+module.exports = app;
---
+++ application/routes/index.js
@@ -0,0 +1,207 @@
+var express = require('express');
+var router = express.Router();
+const db = require('../conf/database.js');
+
+
+
+// Define a route to serve your main HTML page
+router.get('/', (req, res) => {
+    let sql = 'SELECT * FROM tutors';
+    let queryData = [];
+
+    if ((req.query.search || req.query.subject)) {
+        sql += ' WHERE';
+
+        if (req.query.search) {
+            sql += ' name LIKE ?';
+            queryData.push('%' + req.query.search + '%');
+        }
+
+        if (req.query.subject) {
+            if (req.query.search) {
+                sql += ' AND';
+            }
+            sql += ' subject_id = ?';
+            queryData.push(req.query.subject);
+        }
+    }
+    db.query('SELECT * FROM topics', (err, subjects) => {
+        if (err) throw err;
+
+        db.query(sql, queryData, (err, results) => {
+            if (err) throw err;
+            res.render('homepage', { tutors: results, topics: subjects, name: req.query.search,
+                subject: req.query.subject });
+        });
+    });
+});
+
+
+
+router.get('/tutor-application/', (req, res) => {
+    let sql = 'SELECT * FROM tutors';
+    let queryData = [];
+
+    if ((req.query.search || req.query.subject)) {
+        sql += ' WHERE';
+
+        if (req.query.search) {
+            sql += ' name LIKE ?';
+            queryData.push('%' + req.query.search + '%');
+        }
+
+        if (req.query.subject) {
+            if (req.query.search) {
+                sql += ' AND';
+            }
+            sql += ' subject_id = ?';
+            queryData.push(req.query.subject);
+        }
+    }
+    db.query('SELECT * FROM topics', (err, subjects) => {
+        if (err) throw err;
+
+        db.query(sql, queryData, (err, results) => {
+            if (err) throw err;
+            res.render('tutor-application', { tutors: results, topics: subjects, name: req.query.search,
+                subject: req.query.subject });
+        });
+    });
+});
+
+
+
+router.get('/login-form/', (req, res) => {
+    let sql = 'SELECT * FROM tutors';
+    let queryData = [];
+
+    if ((req.query.search || req.query.subject)) {
+        sql += ' WHERE';
+
+        if (req.query.search) {
+            sql += ' name LIKE ?';
+            queryData.push('%' + req.query.search + '%');
+        }
+
+        if (req.query.subject) {
+            if (req.query.search) {
+                sql += ' AND';
+            }
+            sql += ' subject_id = ?';
+            queryData.push(req.query.subject);
+        }
+    }
+    db.query('SELECT * FROM topics', (err, subjects) => {
+        if (err) throw err;
+
+        db.query(sql, queryData, (err, results) => {
+            if (err) throw err;
+            res.render('login-form', { tutors: results, topics: subjects, name: req.query.search,
+                subject: req.query.subject });
+        });
+    });
+});
+
+
+
+router.get('/register-form/', (req, res) => {
+    let sql = 'SELECT * FROM tutors';
+    let queryData = [];
+
+    if ((req.query.search || req.query.subject)) {
+        sql += ' WHERE';
+
+        if (req.query.search) {
+            sql += ' name LIKE ?';
+            queryData.push('%' + req.query.search + '%');
+        }
+
+        if (req.query.subject) {
+            if (req.query.search) {
+                sql += ' AND';
+            }
+            sql += ' subject_id = ?';
+            queryData.push(req.query.subject);
+        }
+    }
+    db.query('SELECT * FROM topics', (err, subjects) => {
+        if (err) throw err;
+
+        db.query(sql, queryData, (err, results) => {
+            if (err) throw err;
+            res.render('register-form', { tutors: results, topics: subjects, name: req.query.search,
+                subject: req.query.subject });
+        });
+    });
+});
+
+
+
+router.get('/dashboard/', (req, res) => {
+    let sql = 'SELECT * FROM tutors';
+    let queryData = [];
+
+    if ((req.query.search || req.query.subject)) {
+        sql += ' WHERE';
+
+        if (req.query.search) {
+            sql += ' name LIKE ?';
+            queryData.push('%' + req.query.search + '%');
+        }
+
+        if (req.query.subject) {
+            if (req.query.search) {
+                sql += ' AND';
+            }
+            sql += ' subject_id = ?';
+            queryData.push(req.query.subject);
+        }
+    }
+    db.query('SELECT * FROM topics', (err, subjects) => {
+        if (err) throw err;
+
+        db.query(sql, queryData, (err, results) => {
+            if (err) throw err;
+            res.render('dashboard', { tutors: results, topics: subjects, name: req.query.search,
+                subject: req.query.subject });
+        });
+    });
+});
+
+
+
+// Route to display tutors and handle search
+router.get('/search-tutors/', (req, res) => {
+    let sql = 'SELECT * FROM tutors';
+    let queryData = [];
+
+    if ((req.query.search || req.query.subject)) {
+        sql += ' WHERE';
+
+        if (req.query.search) {
+            sql += ' name LIKE ?';
+            queryData.push('%' + req.query.search + '%');
+        }
+
+        if (req.query.subject) {
+            if (req.query.search) {
+                sql += ' AND';
+            }
+            sql += ' subject_id = ?';
+            queryData.push(req.query.subject);
+        }
+    }
+    db.query('SELECT * FROM topics', (err, subjects) => {
+        if (err) throw err;
+
+        db.query(sql, queryData, (err, results) => {
+            if (err) throw err;
+            res.render('search-tutors', { tutors: results, topics: subjects, name: req.query.search,
+                subject: req.query.subject });
+        });
+    });
+});
+
+
+
+module.exports = router;
