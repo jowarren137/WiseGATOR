c8d44bc782c8d4a53e3d0ce2edddbfadb0ebc997
karlMoreno <karlcmoreno0811@gmail.com>
2023-12-12 07:02:32 -0800

    Dashboard clean up

--- application/conf/database.js
+++ application/conf/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const db  = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
-  port: '3306',
-  password: '',
+  port: '3307',
+  password: '1234',
   database: 'gatorDB'
 });
 
